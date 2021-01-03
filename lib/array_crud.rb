0f6fbe00f347b8b7bb1f61c5e65ee24f84e7d42e
Emily Nguyen <emilyn0614@gmail.com>
2021-01-03 10:28:15 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,7 +18,7 @@ end
 
 def remove_element_from_end_of_array(array)
   element = ["wow", "I", "am", "really", "learning"]
-  element.shift
+  "wow" = element.shift
 end
 
 def remove_element_from_start_of_array(array)
