7f9f792e035ec1b8c84ae5bcd17d98b2b3488394
Emily Nguyen <emilyn0614@gmail.com>
2021-01-03 10:14:07 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -6,8 +6,7 @@ def create_an_array
  ["kyle", "nick", "jeff", "andy"]
 end
 
-def add_element_to_end_of_array
-  ["kyle", "nick", "jeff", "andy"]
+def add_element_to_end_of_array = ["kyle", "nick", "jeff", "andy"]
   
   
 end
